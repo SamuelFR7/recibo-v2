f8a57363cc64c6f52d8abe1dae867412313c1289
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-28 14:36:57 -0300

    feat: new eslint and prettier config

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+}
+
+module.exports = config
---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+}
+
+module.exports = config
